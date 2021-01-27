f60cb02e7fd51d066863fbc89611943577337bb2
kleo4004 <zkioz4004@gmail.com>
2021-01-27 23:22:26 +0300

    feat: add loading bar

--- rsclone/src/scripts/PreloaderStage.js
+++ rsclone/src/scripts/PreloaderStage.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import Preloader from './Preloader';
 import tileSetImg from '../assets/tileset.png';
 import gameMapJson from '../assets/cityMap1.json';
 import objectsImg from '../assets/objectsAll.png';
@@ -10,6 +11,7 @@ export default class PreloaderStage extends Phaser.Scene {
     }
     preload() {
         this.add.sprite(0, 0, 'imgBG').setOrigin(0);
+        this.preloader = new Preloader(this);
         this.load.spritesheet('tileSet', tileSetImg, {frameWidth: 64, frameHeight: 64});
         this.load.tilemapTiledJSON('tileMap', gameMapJson);
         this.load.atlas('gameObjects', objectsImg, objectsJson);
